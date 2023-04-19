519f6d16e977476aa0a2c71d2f380029b775e628
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-04-19 13:53:09 +0200

    small changes in several files 19.04.23 13:52 Ł.

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,13 +1,36 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
-import { StudentService } from './student.service';
-import { StudentDto } from './dto/student.dto';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
+import { StudentService } from "./student.service";
+import { StudentDto } from "./dto/student.dto";
 
-@Controller('student')
+@Controller("student")
 export class StudentController {
-  constructor(private studentService: StudentService) {}
-
-  @Get('/list-all')
-  signup(@Body() dto: StudentDto) {
-    return this.studentService.listAll();
+  constructor(private studentService: StudentService) {
   }
+
+  @Post("/create")
+  addStudent(@Body() newStudent: StudentDto) {
+    return this.studentService.createStudent(newStudent);
+  }
+
+
+  @Get("/list-all")
+  getAll() {
+    return this.studentService.listAllStudents();
+  }
+
+  @Get("/get-one/:id")
+  getOneStudent( @Param('id') id: string) {
+    return this.studentService.getStudentById(id);
+  }
+
+  @Patch("/update/:id")
+  updateStudent(@Param('id') id: string) {
+    return this.studentService.updateStudent(id);
+  }
+
+  @Delete("/delete/:id")
+  deleteStudent(@Param("id") id: string) {
+    return this.studentService.removeStudent(id);
+  }
+
 }
