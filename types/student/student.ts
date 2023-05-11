557852cc0fdce79bcbeaf88ce324d53eca61dec4
Paulina <paulina.a.gaweda@gmail.com>
2023-05-11 22:08:58 +0200

    Change student list filter

--- types/student/student.ts
+++ types/student/student.ts
@@ -1,9 +1,27 @@
 import { ExpectedTypeWork } from '../../src/enums/expected-type-work';
 import { ExpectedContractType } from '../../src/enums/expected-contract-type';
+import { Student as StudentEntity } from '../../src/student/student.entity';
 
 export interface GetOneStudentResponse {
   id: string;
   email: string;
+  courseCompletion: number;
+  courseEngagement: number;
+  projectDegree: number;
+  teamProjectDegree: number;
+  expectedTypeWork: ExpectedTypeWork;
+  targetWorkCity: string;
+  expectedContractType: ExpectedContractType;
+  expectedSalary: string;
+  canTakeApprenticeship: boolean;
+  monthsOfCommercialExp: number;
+}
+
+export type Student = StudentEntity;
+
+export interface AvailableStudent {
+  id: string;
+  name: string;
   courseCompletion: number;
   courseEngagement: number;
   projectDegree: number;
